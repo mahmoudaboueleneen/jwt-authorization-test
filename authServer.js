6bb9024c9e57282d66592d016fa5149dae2cf8ae
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-07-13 06:43:30 +0300

    Finalized Project

---
+++ authServer.js
@@ -0,0 +1,59 @@
+// This server is responsible for handling JWT create, delete
+// and refresh.
+
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// Should be stored in a DB or cache, but for simplicity's sake it's stored here.
+let refreshTokens = [];
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        const accessToken = generateAccessToken(
+            {
+                username: user.username,
+                scopes: user.scopes,
+                otherStuff: user.otherStuff,
+            }
+        );
+        res.json({ accessToken: accessToken });
+    });
+});
+
+app.post("/login", (req, res) => {
+
+    // authenticate user credentials against DB here...
+
+    const username = req.body.username
+
+    const user = {
+        username: username,
+        scopes: "",
+        otherStuff: "",
+    }
+
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
+});
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s'});
+}
+
+app.listen(4000);
--- server.js
+++ server.js
@@ -1,59 +1,30 @@
 require('dotenv').config();
-
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const app = express();
-
-const jwt = require('jsonwebtoken');
 app.use(express.json());
 
-
 const posts = [
-    {
-        username: "Mahmoud",
-        title: "Post 1"
-    },
-    {
-        username: "Mohammed",
-        title: "Post 2"
-    }
-]
-
+    {username: "Mahmoud", title: "Post 1"}, {username: "Mohammed", title: "Post 2"}
+];
 
 app.get("/posts", authenticateToken, (req, res) => {
     res.json(
-        posts.filter(post => post.username === req.user.name)
+        posts.filter(post => post.username === req.user.username)
     );
-});
-
-
-app.post("/login", (req, res) => {
-   // TODO: Authenticate user
-
-    const username = req.body.username
-    const user = { name: username }
-
-    // User object is serialized into the access token
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken });
-});
+})
 
 function authenticateToken(req, res, next) {
-    // Bearer TOKEN
-    //  [0]    [1]
+    const authHeader = req.headers['authorization'];    // Format of auth header: Bearer TOKEN
+    const token = authHeader && authHeader.split(' ')[1];
 
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split[' '][1];
+    if (token == null) res.sendStatus(401);
 
-    if (token == null)
-        res.sendStatus(401);
-
-    // User object is deserialized from the access token
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err)
-            return res.sendStatus(403);
+        if (err) return res.sendStatus(403);
         req.user = user;
         next();
-    } )
+    });
 }
 
 app.listen(3000);
